c5152a5c7ab2a37f28f4690a1196451edfe8a32e
rahnedacharnysh <charnysh.rahneda@student.ehu.lt>
2024-06-27 17:19:06 +0300

    change back

--- my_project/src/client.c
+++ my_project/src/client.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <unistd.h> // для `write` и `perror`
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include "../include/comm_lib.h"
 
-// Пример функции send_message
-ssize_t send_message(int fd, const char *message, size_t length) {
-    return write(fd, message, length);
-}
+#define FIFO_SERVER "/tmp/myserver.fifo"
 
-int main() {
-    int fd = 1; // Файловый дескриптор для вывода в stdout
-    char message[256] = "Hello, server!";
-    
-    if (send_message(fd, message, strlen(message)) == -1) {
-        perror("send_message");
-    } else {
-        printf("Message sent: %s\n", message);
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <client_fifo>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
+
+    const char *client_fifo = argv[1];
+    int server_fd = client_connect(FIFO_SERVER, client_fifo);
+    if (server_fd == -1) {
+        exit(EXIT_FAILURE);
+    }
+
+    char command[256];
+    while (fgets(command, sizeof(command), stdin) != NULL) {
+        send_message(server_fd, command);
+
+        char buffer[1024];
+        int client_fd = open(client_fifo, O_RDONLY);
+        if (client_fd != -1) {
+            receive_message(client_fd, buffer, sizeof(buffer));
+            printf("Response: %s\n", buffer);
+            close_connection(client_fd);
+        }
+    }
+
+    close_connection(server_fd);
+    unlink(client_fifo);
 
     return 0;
 }
